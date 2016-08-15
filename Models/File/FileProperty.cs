19d4da7c0c8364497ab59f433381c0ef92e59d2e
icepeo <64117190@qq.com>
2016-08-15 10:36:43 +0800

    20160815

---
+++ Models/File/FileProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSUpLoadFile.Models
+{
+    public class FileProperty
+    {
+        public string name { get; set; }
+        public string biz_attr { get; set; }
+        public string ctime { get; set; }
+        public string mtime { get; set; }
+        public int filesize { get; set; }
+        public int filelen { get; set; }
+        public string sha { get; set; }
+        public string access_url { get; set; }
+    }
+}
---
+++ Models/File/UpFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSUpLoadFile.Models
+{
+    /// <summary>
+    /// 上传文件所需参数模版类
+    /// </summary>
+    public class UpFile
+    {
+        public string filename { get; set; } //文件对话框中选定的文件名的字符串
+        public long filenamelen { get; set; }  //文件对话框中选定的文件实际大小
+        public string currentfolder { get; set; } //当前路径
+    }
+}
